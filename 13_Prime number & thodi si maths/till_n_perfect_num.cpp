5c65723ed7790b57f4f0bab537318036a983f0a9
Satyam singh <129842901+Satyam-nitp@users.noreply.github.com>
2024-10-13 15:11:14 +0530

    Add files via upload

---
+++ 13_Prime number & thodi si maths/till_n_perfect_num.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<climits>
+#include<numeric>
+using namespace std;
+void display(vector<int> v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return;
+}
+vector<int> factor(int n){
+    vector<int> v;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            v.push_back(i);
+            if(i!=(n/i)) v.push_back(n/i);
+        }
+    }
+    sort(v.begin(),v.end());
+    return v;
+}
+bool perfect(int n){
+    vector<int> v=factor(n);
+    int sum=0;
+    sum=accumulate(v.begin(),v.end()-1,sum);
+    if(n==sum) return true;
+    return false;
+}
+int main(){
+    int n=10000;
+    for(int i=1;i<=n;i++){
+        if(perfect(i)) cout<<i<<" ";
+    }
+    return 0;
+}
